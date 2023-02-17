a880891d3eadb389a4271a491f32f6472e8d9a14
rubeans <rubensglf@gmail.com>
2023-02-16 23:30:30 -0300

    add a link of my github

--- src/components/containers/header.js
+++ src/components/containers/header.js
@@ -6,13 +6,12 @@ const header = () => {
 
     // The header will be generated here
     header.insertAdjacentHTML('beforeend', `
-    <h1>Ru<span class='beans-txt'>Beans</span></h1>
+    <h1><a href='https://github.com/rubeans'>Ru<span class='beans-txt'>Beans</span></a></h1>
     <p>The best beans out there!</p>
     `)
 
     // Append element to the page
     content.appendChild(header)
-
 }
 
 export default header;

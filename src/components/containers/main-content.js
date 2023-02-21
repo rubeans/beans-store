181f53695f3c4225999294d878a28a9b28070625
rubeans <rubensglf@gmail.com>
2023-02-21 00:15:22 -0300

    switch display from grid to flex on menu page

--- src/components/containers/main-content.js
+++ src/components/containers/main-content.js
@@ -22,7 +22,7 @@ const mainContent = () => {
         <div id="home-content" class="tab-content">
             <div class="container-content">
                 <div class="hours">
-                    <h2 class="heading">Hours</h2>
+                    <h1 class="heading">Hours</h1>
                     <div class="hours-content light-font">
                     <ul class="hours-list">
                         <li class="hour-list">Sunday: 11:30 AM - 11:30 AM</li>
@@ -36,61 +36,53 @@ const mainContent = () => {
                     </div>
                 </div>
                 <div class="location">
-                    <h2 class="heading">Location</h2>
+                    <h1 class="heading">Location</h1>
                     <div class="location-content light-font">
                         <p>Duque de Caxias, Rio De Janeiro - Brazil</p>
                     </div>
                 </div>
                 <div class="about">
-                    <h2 class="heading">Ru<span class='beans-txt'>Beans</span> is a fictional bean's focus restaurant made by <a href="https://github.com/rubeans" target="_blank">Rubens Guilherme</a>. This project was made along with The Odin Project using a tool called Webpack.</h2>
+                    <p class="light-font">Ru<span class='beans-txt'>Beans</span> is a fictional bean's focus restaurant made by <a href="https://github.com/rubeans" target="_blank">Rubens Guilherme</a>. This project was made along with The Odin Project using a tool called Webpack.</p>
                 </div>
             </div>
         </div>
 
         <div id="menu-content" class="tab-content">
             <div class="container-content">
-                <div class="dishes-grid">
-                    <div class="dish">
-                        <div class="img-container">
-                            <img class="food-img" src="https://img.freepik.com/fotos-premium/feijoada-uma-comida-tipica-brasileira_434193-226.jpg?w=2000"/>
-                        </div>
-                        <div class="dish-content">
-                            <div class="price">
-                                <p class="dish-price light-font">$20</p>
-                            </div>
-                            <div class="dish-description">
-                                <h1 class="dish-name">Feijoda</h1>
-                                <p class="dish-p light-font">It consists of a bean stew, usually with meat, and almost always accompanied by rice.</p>
-                            </div>
-                        </div>
+                <div class="dish">
+                    <div class="img-container">
+                        <img class="food-img" src="https://img.freepik.com/fotos-gratis/sortido-close-up-com-deliciosa-comida-brasileira_23-2148739180.jpg?w=900&t=st=1676947750~exp=1676948350~hmac=f54c2bbb0c67517bc47f768296e814a51023070eaaf188184572d18fee1034ca" />
                     </div>
-                    <div class="dish">
-                        <div class="img-container">
-                            <img class="food-img" src="https://img.freepik.com/fotos-premium/feijoada-uma-comida-tipica-brasileira_434193-226.jpg?w=2000"/>
-                        </div>
-                        <div class="dish-content">
-                            <div class="price">
-                                <p class="dish-price light-font">$20</p>
-                            </div>
-                            <div class="dish-content">
-                                <h1 class="dish-name">Feijoda</h1>
-                                <p class="dish-description light-font">It consists of a bean stew, usually with meat, and almost always accompanied by rice.</p>
-                            </div>
-                        </div>
+                    <div class="dish-info">
+                        <h1 class="dish-name heading">Feijoda</h1>
+                        <p class="dish-price">$20</p>
                     </div>
-                    <div class="dish">
-                        <div class="img-container">
-                            <img class="food-img" src="https://img.freepik.com/fotos-premium/feijoada-uma-comida-tipica-brasileira_434193-226.jpg?w=2000"/>
-                        </div>
-                        <div class="dish-content">
-                            <div class="price">
-                                <p class="dish-price light-font">$20</p>
-                            </div>
-                            <div class="dish-content">
-                                <h1 class="dish-name">Feijoda</h1>
-                                <p class="dish-description light-font">It consists of a bean stew, usually with meat, and almost always accompanied by rice.</p>
-                            </div>
-                        </div>
+                </div>
+                <div class="dish">
+                    <div class="img-container">
+                        <img class="food-img" src="https://www.academiaassai.com.br/sites/default/files/feijao_tropeiro.jpg" />
+                    </div>
+                    <div class="dish-info">
+                        <h1 class="dish-name heading">Feijão Tropeiro</h1>
+                        <p class="dish-price">$11</p>
+                    </div>
+                </div>
+                <div class="dish">
+                    <div class="img-container">
+                        <img class="food-img" src="https://www.receitas-sem-fronteiras.com/uploads/media/tutu_de_feij-o-2.jpg?1389805959" />
+                    </div>
+                    <div class="dish-info">
+                        <h1 class="dish-name heading">Tutu de Feijão</h1>
+                        <p class="dish-price">$14</p>
+                    </div>
+                </div>
+                <div class="dish">
+                    <div class="img-container">
+                        <img class="food-img" src="https://media.istockphoto.com/id/521871683/pt/foto/mistura-de-salada-de-feij%C3%A3o.jpg?s=612x612&w=0&k=20&c=hyjJCfnPzDC0RoJoIQyjRNXx9U9Sg8xoNDfoTu3P1cM=" />
+                    </div>
+                    <div class="dish-info">
+                        <h1 class="dish-name heading">Bean Salad</h1>
+                        <p class="dish-price">$9</p>
                     </div>
                 </div>
             </div>
